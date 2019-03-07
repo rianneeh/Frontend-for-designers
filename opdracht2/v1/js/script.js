0704a2de0d07f47e8a3bed676c9cb983d44b944c
Rianne <rianne285@gmail.com>
2019-03-07 10:43:01 +0100

    Comments

--- opdracht2/v1/js/script.js
+++ opdracht2/v1/js/script.js
@@ -4,46 +4,59 @@
 
 /*eslint 'no-console': 0*/
 
+/*De twee knoppen krijgen elk een variabele*/
 var previous = document.querySelector('#previous');
 var next = document.querySelector('#next');
 
+/*De variabele huidigeImg wordt gebruikt om te tellen welke img moet worden weergeven*/
 var huidigeImg = 1;
 
 function volgendeImg(sel) {
-	weergeefImg(huidigeImg += sel);
+	weergeefImg(huidigeImg += sel); /*huidige img wordt 1 bij opgeteld en met die nieuwe waarde word de functie weergeefImg uitgevoerd*/
 }
 
 function vorigeImg(sel) {
-	weergeefImg(huidigeImg -= sel);
+	weergeefImg(huidigeImg -= sel); /*huidige img wordt 1 afgehaald en met die nieuwe waarde word de functie weergeefImg uitgevoerd*/
 }
 
 function weergeefImg(sel) {
 	var i;
+	/*alle images worden in een array gezet/geteld*/
 	var alleImg = document.querySelectorAll('img');
+	/*alle dots worden in een array gezet/geteld*/
 	var alleDots = document.querySelectorAll('.dot');
+	/*als de waarde van huidigeImg hoger word dan het totaal aantal Img, gaat de waarde terug naar 1(de eerste foto)*/
 	if (sel > alleImg.length) {
 		huidigeImg = 1
 	};
+	/*als de waarde van huidigeImg lager word dan 1, word de waarde het toaal aantal Img (de laatste foto)*/
 	if (sel < 1) {
 		huidigeImg = alleImg.length
 	};
+	/*de class show word van alle Img verwijdert*/
 	for (i = 0; i < alleImg.length; i++) {
 		alleImg[i].classList.remove('show');
 	}
+	/*de active show word van alle dots verwijdert*/
 	for (i = 0; i < alleDots.length; i++) {
 		alleDots[i].classList.remove('active');
 	}
+	/*de img en dot die op dat moment moeten worden weergeven krijgen de juiste class, -1 omdat de array begint bij 0*/
 	alleImg[huidigeImg - 1].classList.add('show');
 	alleDots[huidigeImg - 1].classList.add('active');
 }
 
+/*Als de eerste knop word geklikt, word de functie vorigeImg uitgevoerd*/
 previous.addEventListener('click', function () {
 	vorigeImg(1);
 });
+
+/*Als de tweede knop word geklikt, word de functie volgendeImg uitgevoerd*/
 next.addEventListener('click', function () {
 	volgendeImg(1);
 });
 
+/*als de pijl terug word aangeklikt, word vorigeImg uitgevoerd, als de pijl verder word aangeklikt, word volgendeImg uitgevoerd*/
 document.addEventListener('keydown', function (event) {
 	if (event.keyCode === 39) {
 		volgendeImg(1);
